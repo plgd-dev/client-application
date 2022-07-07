1ac22eabcf335cebf868571de3083a23ea646e4c
Patrik Matiaško <patrik.matiasko@gmail.com>
2022-07-07 21:07:51 +0200

    Print current version in log and ui (endpont)

--- web/src/components/footer/footer.js
+++ web/src/components/footer/footer.js
@@ -1,21 +1,31 @@
 /* eslint-disable react/jsx-no-target-blank */
-import { memo } from 'react'
+import { memo, useState } from 'react'
 import { useIntl } from 'react-intl'
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 import Tooltip from 'react-bootstrap/Tooltip'
+import Popover from 'react-bootstrap/Popover'
+import PopoverContent from 'react-bootstrap/PopoverContent'
 
 import { messages as t } from './footer-i18n'
 import './footer.scss'
 import { useAppConfig } from '@/containers/app'
 import { copyToClipboard } from '@/common/utils'
 import { showSuccessToast } from '@/components/toast'
+import { Display } from '@/components/display'
 
 export const Footer = memo(() => {
   const { formatMessage: _ } = useIntl()
-  const { version } = useAppConfig()
+  const { buildInformation } = useAppConfig()
+  const [show, setShow] = useState(false)
+  const handleOnMouseEnter = () => {
+    setShow(true)
+  }
+  const handleOnMouseLeave = () => {
+    setShow(false)
+  }
 
   const copyVersion = () => {
-    copyToClipboard(version)
+    copyToClipboard(buildInformation.version)
     showSuccessToast({
       title: _(t.done),
       message: _(t.copied),
@@ -25,21 +35,60 @@ export const Footer = memo(() => {
   return (
     <footer id="footer">
       <div className="left">
-        <OverlayTrigger
-          placement="top"
-          overlay={
-            <Tooltip className="plgd-tooltip" id="footer-version">
-              {_(t.clickToCopy)}
-            </Tooltip>
-          }
-        >
-          <span className="copy" onClick={copyVersion}>
-            <i className={`fas fa-copy m-r-10`} />
-          </span>
-        </OverlayTrigger>
-        <span className="copy" onClick={copyVersion}>
-          {version}
-        </span>
+        <Display when={!!buildInformation}>
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip className="plgd-tooltip" id="footer-version">
+                {_(t.clickToCopy)}
+              </Tooltip>
+            }
+          >
+            <span className="copy" onClick={copyVersion}>
+              <i className={`fas fa-copy m-r-10`} />
+            </span>
+          </OverlayTrigger>
+
+          <OverlayTrigger
+            placement="top-start"
+            show={show}
+            delay={{ hide: 400 }}
+            overlay={
+              <Popover
+                id="popover-positioned-top"
+                className="plgd-popover"
+                onMouseEnter={handleOnMouseEnter}
+                onMouseLeave={handleOnMouseLeave}
+              >
+                <PopoverContent>
+                  <div className="footerBuildInfo">
+                    <div className="line">
+                      <div className="attr">Build time:</div>
+                      <div className="val">{buildInformation.buildDate}</div>
+                    </div>
+                    <div className="line">
+                      <div className="attr">Commit hash:</div>
+                      <a
+                        className="val"
+                        href={`https://github.com/plgd-dev/client-application/commit/${buildInformation.commitHash}`}
+                        target="_blank"
+                      >
+                        {buildInformation.commitHash}
+                      </a>
+                    </div>
+                  </div>
+                </PopoverContent>
+              </Popover>
+            }
+          >
+            <span
+              onMouseEnter={handleOnMouseEnter}
+              onMouseLeave={handleOnMouseLeave}
+            >
+              {buildInformation.version}
+            </span>
+          </OverlayTrigger>
+        </Display>
       </div>
       <div className="right">
         <a
