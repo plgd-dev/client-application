50c6381ed43a89b899c93fdd29e34849f3d6a684
Patrik Matiaško <patrik.matiasko@gmail.com>
2022-08-27 22:27:59 +0200

    Devices TS components

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,5 +1,5 @@
 import { Switch, Route } from 'react-router-dom'
-import { DevicesListPage } from '@/containers/devices'
+import DevicesListPage  from '@/containers/devices/List/DevicesListPage'
 import DevicesDetailsPage from '@/containers/devices/Detail/DevicesDetailsPage'
 import { NotFoundPage } from '@/containers/not-found-page'
 
